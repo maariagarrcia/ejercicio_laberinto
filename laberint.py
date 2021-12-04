d0e8a38d0512426d285714a6d656bd3dc79a57e7
maariagarrcia <mrrmn@myuax.com>
2021-12-04 12:29:28 +0100

    funciones

--- laberint.py
+++ laberint.py
@@ -1,3 +1,30 @@
+import os
+
+#### F U N C I O N E S ####
+# Limpiar la terminal
+# No funciona al ejecutar en ventana interactiva :(
+def clear():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def crear_laberintoo_vacio(dim):
+
+def ajustar_muros_laberinto(l,muros):
+
+def mostrar_laberinto(titulo,laberinto):
+
+
+
+
+
+
+
+
+
+
+
 #### I N I C I O    P R O G R A M A  ####
 # Creacion del laberinto sin muros
 laberinto= crear_laberinto_vacio(dimension)
