5527b6c4fd47b25eb7bd76c004257f64191b8c82
maariagarrcia <mrrmn@myuax.com>
2021-12-06 16:25:50 +0100

    cmentarios

--- laberint.py
+++ laberint.py
@@ -17,9 +17,10 @@ import colorama
 ####
 
 
-# Limpiar la terminal
-# No funciona al ejecutar en ventana interactiva 
+
 def clear():
+    # Limpiar la terminal
+    # No funciona al ejecutar en ventana interactiva 
     if os.name == "nt":
         os.system("cls")
     else:
@@ -52,8 +53,10 @@ def crear_laberinto_vacio(dim):
     return l
 
 
-# Pone las "X" en el laberinto donde esta especificado que debe haber un muro
+
 def ajustar_muros_laberinto(l, muros):
+    # Pone una  "X" en las casilla donde esta especificado
+    # que debe haber un muro --> Casillas "NO transitables"
     for ladrillo in muros:
         l[ladrillo[0]][ladrillo[1]] = "X"
 
@@ -78,28 +81,31 @@ def mostrar_laberinto(titulo, laberinto):
     print(colorama.Back.BLACK + colorama.Fore.WHITE)
 
 def poner_casilla_entrada(lab,entrada):
+    # Pone una  "E" en la casilla de entrada al laberinto
     lab[entrada[0]][entrada[1]]="E"
     
 
 def poner_casilla_salida(lab,salida):
+    # Pone una "S" en la casilla de salida del laberinto
     lab[salida[0]][salida[1]]="S"
     
 
 
-def ajustar_camino_sobre_laberinto(laberinto, entrada, salida, camino_salida):
-# Por cada paso que no sea la entrada/salida pondremos "·" para 
-# visualizarlo de una forma mas grafica.
-# Para ello usamos la lista de "camino_salida" que contiene las 
-# coordenadas de todas las celdas q forman partte del camino de salida.
-# A cada celda que forma parte del camino de salida la he denominado "paso"
+def ajustar_camino_sobre_laberinto(laberinto,entrada,salida,camino_salida):
+    # Por cada paso que no sea la entrada/salida pondremos "·" para 
+    # visualizarlo de una forma mas grafica.
+    # Para ello usamos la lista de "camino_salida" que contiene las 
+    # coordenadas de todas las celdas q forman partte del camino de salida.
+    # A cada celda que forma parte del camino de salida la he denominado "paso"
     for paso in camino_salida:
         if(paso != entrada) and (paso != salida):
             laberinto[paso[0]][paso[1]] = "·"
 
 
-# Creacion del diccionario para guardar todas las casillas usadas
-# y para no volverlas a visitar
+
 def crear_diccionario_casillas(dim):
+    # Creacion del diccionario para guardar todas las casillas usadas
+    # y para no volverlas a visitar
     # Creamos un diccionario con todas las casillas del 
     # laberinto y un indicador booleano (False) de q no se ha usado
     
@@ -108,26 +114,23 @@ def crear_diccionario_casillas(dim):
     for num_fila in range(0,dim):
         for num_columna in range(0,dim):
             #Añadir una nueva casilla al diccionario
-            casillas[str(num_fila)+ "-" + str(num_columna)]=False
+            casillas[str(num_fila)+"-"+str(num_columna)] = False
     
     return casillas
 
-
-
-
-def paso_posible(laberinto, casillas_usadas, dim, num_fila, num_columna):
-   # El objetivo de esta funcion es validar si una casilla
-# puede formar parte del camino de salida.
-# En principio para cada casilla son posibles cuatro pasos, 
-# (arriba,abajo,derecha,izquierda),
-# pero, para que la casilla forme parte hay que descartar los siguientes casos:
-# - Que la casilla esté fuera del laberinto
-# - Que en la casilla haya un muro ("X")
-# - Que sea la casilla de entrada al laberintoo ("E")
-# - Que la casilla haya  sido visitada previamente. Para esto
-# usaremos el diccionario casillas_usadas
-#False --> opcion no valida
-# True --> es el paso posible 
+def paso_posible(laberinto, casillas_usadas, dim, num_fila, num_columna): 
+    # El objetivo de esta funcion es validar si una casilla
+    # puede formar parte del camino de salida.
+    # En principio para cada casilla son posibles cuatro pasos, 
+    # (arriba,abajo,derecha,izquierda),
+    # pero, para que la casilla forme parte hay que descartar los siguientes casos:
+    # - Que la casilla esté fuera del laberinto
+    # - Que en la casilla haya un muro ("X")
+    # - Que sea la casilla de entrada al laberintoo ("E")
+    # - Que la casilla haya  sido visitada previamente. Para esto
+    # usaremos el diccionario casillas_usadas
+    #False --> opcion no valida
+    # True --> es el paso posible 
    
     # Comprobar si fila fuera de rango
     if (num_fila < 0) or (num_fila >= dim):
@@ -185,31 +188,49 @@ def marcar_casilla_como_usada(casillas_usadas,casilla):
 
 
 def buscar_camino_salida(laberinto,casillas_usadas,dim):
-    # Damos por hecho que la entrada siempre está  en sus coordenadas 0,0.
+    # Funcion principal del programa y su objetivo es
+    # calcular un camino de salida entre los que pueda haber.
+    #Empezamos directamente en la casilla de entrada
+    # Damos por hecho que la entrada siempre está en la esquina superior izquierda --> coordenadas 0,0.
     pos_actual = (0,0)
-    camino= [(0,0)]
+    camino= [(0, 0)]
+    nueva_direccion= ""
+    direccion_salida= ["ENTRADA"]
     casillas_usadas["0-0"] = True
 
+    # Indicamos que todavia no hemos encontrado el camino de salida
+    # El bucle while iterará mientras no se haya encontrado la salida y mientras existan
+    # celdas que explorar si conducen a una salida.
     salida = False
     while (not salida) and  (len(camino)>0):
+        #Damos un paso en busca de la salida
         nuevo_paso = dar_un_paso(laberinto, casillas_usadas, dim, pos_actual)
         if (len(nuevo_paso) == 0):
+            # No hemos podido desde la celda actual -> len(nuevo_paso) == 0
+
             # Eliminar la ultima posicion del camino porque sabemos  que no 
             # conduce a la salida
             camino.pop()
+            direccion_salida.pop()
 
-            # Convertir el ultimo paso del camino en posicion actual
-            # Al ultimo elemento de una  lista se accede con el indice -1
+            # Retrocedemos un paso en el camino: Convertir el ultimo paso del camino en posicion actual
+            # Al ultimo elemento de una lista se accede con el indice -1
             pos_actual = camino[-1]
         else:
+            # Hemos encontrado una celda para explorar si nos lleva a la salida
+            
+            # Añadimos la nueva celda al camino de salida
             camino.append(nuevo_paso)
+            direccion_salida.append(nueva_direccion)
+
+            # Marcamos la casilla como usada para no volverla a explorar
             marcar_casilla_como_usada(casillas_usadas, nuevo_paso)
             pos_actual=nuevo_paso
 
         # Marcar casilla como usada en el diccionario
         salida= laberinto[pos_actual[0]][pos_actual[1]] == "S" 
 
-    return camino
+    return camino, direccion_salida
 
 
 
@@ -222,12 +243,17 @@ muros = ((0, 1), (0, 2), (0, 3), (0, 4), (1, 1), (2, 1),
         (2, 3), (3, 3), (4, 0), (4, 1), (4, 2), (4, 3))
 
 # Ancho y alto del laberinto ya que se supone cuadrado
-dimension= 6
+dimension= 5
 
 # Posicion de entrada y salida del laberinto
 entrada=(0,0)
-salida=(5,5)
+salida=(4,4)
 
+# Camino de slida --> Lista de coordenads de casillas que conducen a la salida
+caminoo_salida= []
+
+# Direccion de salida --> Instrucciones para ir hasta la salida en forma de direccion
+direccion_salida= []
 
 ####  I N I C I O    P R O G R A M A  ####
 clear()
