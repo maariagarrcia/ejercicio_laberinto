6bed46f06f6d8b624cc35b3ac2be1c476556275f
maariagarrcia <mrrmn@myuax.com>
2021-12-06 12:16:41 +0100

    funcion y algunos errores

--- laberint.py
+++ laberint.py
@@ -46,21 +46,22 @@ def crear_laberinto_vacio(dim):
     
     return l
 
+
 # Pone las "X" en el laberinto donde esta especificado que debe haber un muro
 def ajustar_muros_laberinto(l, muros):
-    for m in muros:
-        l[m[0]] [m[1]] = "X"
+    for ladrillo in muros:
+        l[ladrillo[0]][ladrillo[1]] = "X"
 
 def mostrar_laberinto(titulo, laberinto):
-    print("*** " + titulo + " ***")
+    print(colorama.Fore.YELLOW + "*** " + titulo + " ***")
     for fila in laberinto:
         for celda in fila:
             if (celda == " "):
                 print(colorama.Back.WHITE + "  ",end="")
             elif(celda== "E"):
-                print(colorama.Back.GREEN +" E",end="")
+                print(colorama.Back.GREEN +"E ",end="")
             elif(celda== "S"):
-                print(colorama.Back.GREEN + " S", end="")
+                print(colorama.Back.GREEN + "S ", end="")
             elif(celda=="X"):
                 print(colorama.Back.RED + " X", end="")
             elif(celda=="·"):
@@ -80,9 +81,9 @@ def poner_casilla_salida(lab,salida):
     
 
 # Por cada paso que no sea la entrada/salida pondremos "·"
-def ajustar_camino_sobre_laberinto(laberinto,entrada,salida,camino_salida):
+def ajustar_camino_sobre_laberinto(laberinto, entrada, salida, camino_salida):
     for paso in camino_salida:
-        if(paso != entrada) and (paso!=salida):
+        if(paso != entrada) and (paso != salida):
             laberinto[paso[0]][paso[1]] = "·"
 
 
@@ -97,7 +98,8 @@ def crear_diccionario_casillas(dim):
     for num_fila in range(0,dim):
         for num_columna in range(0,dim):
             #Añadir una nueva casilla al diccionario
-            casillas[str(num_fila)+"-" + str(num_columna)]=False
+            casillas[str(num_fila)+ "-" + str(num_columna)]=False
+    
     return casillas
 
 
@@ -112,7 +114,7 @@ def crear_diccionario_casillas(dim):
 # usaremos el diccionario casillas_usadas
 #False --> opcion no valida
 # True --> es el paso posible 
-def paso_posible(laberinto, casillas_usadas,dim,num_fila,num_columna):
+def paso_posible(laberinto, casillas_usadas, dim, num_fila, num_columna):
     # Comprobar si fila fuera de rango
     if (num_fila<0) or (num_columna>=dim):
       return False
@@ -162,10 +164,39 @@ def dar_un_paso(laberinto,casillas_usadas,dim,pos_actual):
 def marcar_casilla_como_usada(casillas_usadas,casilla):
     num_fila=casilla[0]
     num_columna=casilla[1]
-    casillas_usadas[str(num_fila)+ "-"+ str(num_columna)]= True
+    casillas_usadas[str(num_fila)+ "-" + str(num_columna)]= True
 
 
 def buscar_camino_salida(laberinto,casillas_usadas,dim):
+    # Damos por hecho que la entrada siempre está  en sus coordenadas 0,0.
+    pos_actual = (0,0)
+    camino= [(0,0)]
+    casillas_usadas["0-0"] = True
+
+    salida = False
+    while (not salida) and  (len(camino)>0):
+        nuevo_paso = dar_un_paso(laberinto, casillas_usadas, dim, pos_actual)
+        if (len(nuevo_paso) == 0):
+            # Eliminar la ultima posicion del camino porque sabemos  que no 
+            # conduce a la salida
+            camino.pop()
+
+            # Convertir el ultimo paso del camino en posicion actual
+            # Al ultimo elemento de una  lista se accede con el indice -1
+            pos_actual = camino[-1]
+        else:
+            camino.append(nuevo_paso)
+            marcar_casilla_como_usada(casillas_usadas, nuevo_paso)
+            pos_actual=nuevo_paso
+
+        # Marcar casilla como usada en el diccionario
+        salida= laberinto[pos_actual[0]][pos_actual[1]] == "S" 
+
+    return camino
+
+
+
+
 
 #### V A R I A B L E S   G L O B A L E S ####
 
@@ -174,31 +205,31 @@ muros = ((0, 1), (0, 2), (0, 3), (0, 4), (1, 1), (2, 1),
         (2, 3), (3, 3), (4, 0), (4, 1), (4, 2), (4, 3))
 
 # Ancho y alto del laberinto ya que se supone cuadrado
-dimension= 5
+dimension= 6
 
 # Posicion de entrada y salida del laberinto
 entrada=(0,0)
-salida=(4,4)
+salida=(5,5)
 
 
 ####  I N I C I O    P R O G R A M A  ####
 clear()
 
-# Creacion del laberinto sin muros
 laberinto= crear_laberinto_vacio(dimension)
-#mostrar_laberinto("Laberinto vacio", laberinto)
+# mostrar_laberinto("Laberinto vacio", laberinto)
 
-# Creacion del laberinto con muros
 ajustar_muros_laberinto(laberinto,muros)
-poner_casilla_entrada(laberinto,0,0)
-poner_casilla_salida(laberinto,4,4)
-#mostrar_laberinto("Laberinto con muros", laberinto)
-
+# mostrar_laberinto("Laberinto con muros", laberinto)
 
 mostrar_laberinto("Laberinto completo", laberinto)
+poner_casilla_entrada(laberinto,entrada)
+poner_casilla_salida(laberinto,salida)
+
 casillas_usadas = crear_diccionario_casillas(dimension)
+
 camino_salida = buscar_camino_salida(laberinto, casillas_usadas, dimension)
-ajustar_camino_sobre_laberinto(laberinto,casillas_usadas,dimension)
-mostrar_laberinto("Caminoo de salida",laberinto)
+
+ajustar_camino_sobre_laberinto(laberinto,entrada,salida,camino_salida)
+mostrar_laberinto("Camino de salida",laberinto)
 
 print(camino_salida)
